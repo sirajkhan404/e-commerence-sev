3a9f2dacc983b6b33eae078db211a203dbe35c14
sirajkhan <sirajkhank819@gmail.com>
2026-07-24 16:48:33 +0500

    index.js

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const orders = require("./routes/orders");
 
 connectDB();
 
+// app.use(cors({
+//     origin: ["http://localhost:5173", "https://e-commerence-fe.vercel.app"],
+//     credentials: true
+// }));
+
 app.use(cors({
-    origin: ["http://localhost:5173", "https://e-commerence-fe.vercel.app"],
+    origin: process.env.NODE_ENV === 'production'
+        ? "https://e-commerence-fe.vercel.app"
+        : true, // Development mein har origin allow kar dega
     credentials: true
 }));
 
